4c4db3b37d7be2a435db9e8c325fc95b227e967f
Adi Winata <adheegm@gmail.com>
2019-01-05 13:39:52 +0700

    deploy config

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-//Install express server
 const express = require('express');
-const http = require('http');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname + '/dist')));
+app.use(express.static('./dist/aw-controls-demo'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'))
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname,'/dist/aw-controls-demo/index.html'));
 });
 
-const port = process.env.PORT || '8080';
-app.set('port', port);
-
-// Start the app by listening on the default Heroku port
-//app.listen(process.env.PORT || 8080);
-
-const server = http.createServer(app);
-
-server.listen(port, () => console.log('Running'));
+app.listen(process.env.PORT || 8080);
